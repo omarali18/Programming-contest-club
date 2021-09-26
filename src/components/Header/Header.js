fb4dc6c7b7a114ad42713d89f499456057399611
omarali18 <omarali1814000@gmail.com>
2021-09-26 08:33:34 +0600

    header component add

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div>
+                <h1 className="title">Programming contest by <span className="club">Computing club</span></h1>
+                <p>If you want to take part in this contest, click on Enroll Now.</p>
+                <h3>Total budget: 50 million</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
