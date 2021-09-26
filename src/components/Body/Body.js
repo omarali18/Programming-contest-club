e0b3da301c5ec84ee38a3df97008cc2d0639ff07
omarali18 <omarali1814000@gmail.com>
2021-09-26 11:17:56 +0600

    competitors card added

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -15,14 +15,17 @@ const Body = () => {
         <div>
             <div className="competitor-container">
                 <div className="competitor-side">
+                    {/* show computing club's student  */}
                     {
                         competitors.map(competitor => <Competitors
                             competitor={competitor}
                             key={competitor.id} />)
                     }
                 </div>
+                {/* add contest student  */}
                 <div className="card-side">
-                    <h3>Entry fee total</h3>
+                    <h2><i class="fas fa-user"></i> Student added</h2>
+                    <h2>Entry fee total</h2>
                 </div>
             </div>
         </div>
--- src/components/Competitors/Competitors.js
+++ src/components/Competitors/Competitors.js
@@ -3,24 +3,23 @@ import "./Competitors.css"
 
 const Competitors = (props) => {
     const { name, age, role, id, img, email, entryFee, department } = props.competitor
-    console.log(props.competitor);
     return (
-        <div className="competitor-card">
-            <div className="card">
-                <div className="competitor-img">
-                    <img src={img} alt="" />
-                </div>
-                <div className="competitor-detail">
-                    <h2>Name: {name}</h2>
-                    <p>ID: {id}</p>
-                    <p>Role: {role}</p>
-                    <p>Department: {department}</p>
-                    <p>Email: {email}</p>
-                    <p>Age: {age}</p>
-                    <p>EntryFee: {entryFee}</p>
-                    <button>Inrole</button>
-                </div>
+        <div className="competitor-cart">
+
+            <div className="competitor-img">
+                <img src={img} alt="" />
             </div>
+            <div className="competitor-detail">
+                <h2>Name: {name}</h2>
+                <p>ID: {id}</p>
+                <p>Role: {role}</p>
+                <p>Department: {department}</p>
+                <p>Email: {email}</p>
+                <p>Age: {age}</p>
+                <p>EntryFee: {entryFee}</p>
+                <button>participate now <i className="fas fa-arrow-circle-right"></i></button>
+            </div>
+
         </div>
     );
 };
