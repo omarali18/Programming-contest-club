32694043f0aa68302a43f2e12ed4171929368964
omarali18 <omarali1814000@gmail.com>
2021-12-04 16:39:52 +0600

    test commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="header">
             <div>
-                <h1 className="title">Programming contest by <span className="club">Computing club</span></h1>
+                <h1 className="title">Programming contest by <span className="club">Computing club.</span></h1>
                 <p>If you want to take part in this contest, click on Enroll Now.</p>
                 <h3>Total budget: 50 million</h3>
             </div>
